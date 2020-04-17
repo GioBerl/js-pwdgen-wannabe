689335dd090c9e0a5d68a3f3dfbd4c5389cc554b
Gio <giovanni.berl1@gmail.com>
2020-04-17 19:59:54 +0200

    aggiunta qualche animazione

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // chiedere all'utente il suo nome, il suo cognome e il suo colore preferito e con queste informazioni creare una (insicurissima) password così formata: [nome][cognome][colore preferito]20
 
-//chiedere nome utente
+//chiedere nome
 var nome = prompt('dimmi il tuo nome');
 
 //cognome
@@ -9,5 +9,16 @@ var cognome = prompt('e il tuo cognome?');
 //colore preferito
 var colorePreferito = prompt('e il tuo colore preferito?');
 
-//creare la password e stamparla su html
-document.writeln('password creata: ' + nome + cognome + colorePreferito + '20');
+//creare la password
+var passwordGenerata = nome + cognome + colorePreferito + '20';
+
+// inserirla nell'html
+document.getElementById('password').innerHTML = passwordGenerata;
+document.getElementById('generate').innerHTML = 'password generata: ';
+document.getElementById('title').innerHTML = 'Password Generator';
+
+
+// animazioni
+document.getElementById('title').setAttribute('class', 'title-animation');
+document.getElementById('password').setAttribute('class', 'password-animation');
+document.getElementById('generate').setAttribute('class', 'generate-animation');
